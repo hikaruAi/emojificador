b55bddee187574338d8b71f833fbeaf576c0349d
juan3d <contacto.juanmendoza@gmail.com>
2017-09-03 18:46:59 -0600

    Added the B and P Fixed issue with banned words

--- emojificador.py
+++ emojificador.py
@@ -10,7 +10,7 @@ banned=["de","con","la","el","ellos","mi","al","un","los","las","que","se","es",
 for l in lines:
 	listapalabras=l.split(" ")
 	for p in listapalabras:
-		if len(p)<2 or p.lower() in banned:
+		if len(p)<1 or p.lower() in banned:
 			listapalabras.remove(p)
 	listapalabras[-1]=listapalabras[-1].replace("\n","")
 	newlines.append(listapalabras)
@@ -23,6 +23,7 @@ def emojizar(text):
 		if palabra.lower() in banned:
 			print(palabra, "-> banned")
 			found=True
+			tf+= palabra+" "
 		print("Palabra actual: ",palabra)
 		for linea in newlines:
 			if found:
@@ -37,13 +38,14 @@ def emojizar(text):
 					found=True
 		if found==False:
 			tf+=palabra+" "
-	
+	tf= tf.replace("b","🅱").replace("B","🅱").replace("p", "🅿").replace("P", "🅿").replace("gg","🅱🅱")
 	return tf
 	print("Time:", time.time()-t0)
 
 if __name__=="__main__":
 	try:
 		import clipboard
+		#t="""La computadora dió una señal roja de atención refiriendose a un error en la información introducida o con los resultados obtenidos al compararse con las normas establecidas. La oficina de mantenimiento realiza un chequeo, pero la computadora estaba en perfecto estado. El director de operaciones de IBM preguntó cual era el problema entonces siendo la respuesta: “ Detectamos que falta un día en el universo del tiempo transcurrido en la historia”."""
 		t=str(clipboard.paste())
 		print("Text: ",t)
 		if len(t)<1:
